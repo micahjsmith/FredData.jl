916a84b169cea3d2fd33f075a7c26b8fcac2cc32
Micah Smith <micahjsmith@gmail.com>
2015-11-05 21:27:07 -0500

    Parse series/observations response

--- src/series.jl
+++ src/series.jl
@@ -2,17 +2,41 @@
 """
 function get_data(f::Fred, series::ASCIIString)
     # Setup
-    url = get_api_url(f) * "series"
-    key = get_api_key(f)
-    query=Dict()
+    metadata_url = get_api_url(f) * "series"
+    obs_url      = get_api_url(f) * "series/observations"
+    key          = get_api_key(f)
+    query        = Dict()
 
     # Add query parameters
     query["series_id"] = series
-    query["api_key"] = key
+    query["api_key"]   = key
     query["file_type"] = "json"
 
-    response = get(url; query=query)
-    response_json = Requests.json(response)
+    # Query metadata
+    metadata = get(url; query=query)
+    metadata_json = Requests.json(metadata)
 
-    return response_json
+    # Query observations
+    obs = get(url; query=query)
+    obs_json = Request.json(obs)
+
+    df = parse_observations(obs_json["observations"])
+
+    return nothing
+end
+
+# obs is a vector, of which each element is a dict with four fields,
+# - realtime_start
+# - realtime_end
+# - date
+# - value
+function parse_observations(obs::Vector)
+    n_obs = length(obs["observations"])
+    values = Vector{Float64}(n_obs)
+    dates  = Vector{Date}(n_obs)
+    for (i, x) in enumerate(obs["series"])
+        values[i] = parse(Float64, x["value"])
+        dates[i]  = Date(x["date"], "yyyy-mm-dd")
+    end
+    return DataFrame(dates=dates, values=values)
 end
