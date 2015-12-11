5a0d5f9b18a93e1e3f66f7b19e77a8da49aeb9d2
Micah Smith <micahjsmith@gmail.com>
2015-12-11 17:26:08 -0500

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
 using FredData
 using Base.Test
 
-@test 1==1
+# Normal usage
+f = Fred()
+a = get_data(f, "GDPC1")
+a = get_data(f, "GDPC1"; units="chg", vintage_dates="2015-01-01")
+
+# Bad requests
+@test_throws ErrorException get_data(f, "GDPC1"; limit="foo")
+@test_throws ErrorException get_data(f, "GDPC1"; vintage_dates="bar")
