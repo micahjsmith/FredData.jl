c9969b42a9b8f9d1d533fd48981512a017914ebb
Micah Smith <micahjsmith@gmail.com>
2015-12-11 15:20:16 -0500

    Option to load API key from ENV

--- src/FredData.jl
+++ src/FredData.jl
@@ -8,16 +8,18 @@ import Requests: get
 import JSON
 
 export Fred, get_api_url, set_api_url!, get_api_key
-export FredSeries, id, title, units_short, units, seas_adj_short, seas_adj, freq_short, 
+export FredSeries, id, title, units_short, units, seas_adj_short, seas_adj, freq_short,
        freq, realtime_start, realtime_end, last_updated, notes, trans_short, df
 export get_data
 
-const MAX_ATTEMPTS = 3
-const FIRST_REALTIME = Date(1776,07,04)
-const LAST_REALTIME  = Date(9999,12,31)
+const MAX_ATTEMPTS       = 3
+const FIRST_REALTIME     = Date(1776,07,04)
+const LAST_REALTIME      = Date(9999,12,31)
 const EARLY_VINTAGE_DATE = "1991-01-01"
-const DEFAULT_API_URL = "http://api.stlouisfed.org/fred/"
-const API_KEY_LENGTH  = 32
+const DEFAULT_API_URL    = "http://api.stlouisfed.org/fred/"
+const API_KEY_LENGTH     = 32
+const KEY_ENV_NAME       = "FRED_API_KEY"
+const KEY_FILE_NAME      = ".freddatarc"
 
 # Fred connection type
 """
@@ -25,8 +27,9 @@ A connection to the Fred API.
 
 Constructors
 ------------
-- `Fred()`                     # Default connection, reading from ~/.freddatarc
-- `Fred(key::AbstractString)`  # Custom connection
+- `Fred()`: Key detected automatically. First, looks for the environment variable
+    FRED_API_KEY, then looks for the file ~/.freddatarc.
+- `Fred(key::AbstractString)`: User specifies key
 
 Arguments
 ---------
@@ -43,24 +46,28 @@ end
 Fred(key) =  Fred(key, DEFAULT_API_URL)
 function Fred()
     key = ""
-    try
-        open(joinpath(homedir(),".freddatarc"), "r") do f
-            key = readall(f)
+    if KEY_ENV_NAME in keys(ENV)
+        key = ENV[KEY_ENV_NAME]
+    elseif isfile(joinpath(homedir(), KEY_FILE_NAME))
+        open(joinpath(homedir(), KEY_FILE_NAME), "r") do file
+            key = readall(file)
         end
         key = rstrip(key)
-        @printf "API key loaded.\n"
-    catch err
-        @printf STDERR "Add Fred API key to ~/.freddatarc\n"
-        rethrow(err)
+    else
+        error("FRED API Key not detected.")
     end
+
+    @printf "API key loaded.\n"
 
     # Key validation
     if length(key) > API_KEY_LENGTH
         key = key[1:API_KEY_LENGTH]
         warn("Key too long. First ", API_KEY_LENGTH, " chars used.")
+    elseif length(key) < API_KEY_LENGTH
+        error("Invalid FRED API key: ", key, ". Key too short.")
     end
     if !isxdigit(key)
-        error("Invalid FRED API key: ", key)
+        error("Invalid FRED API key: ", key, ". Invalid characters.")
     end
 
     return Fred(key)
@@ -142,16 +149,16 @@ df(f::FredSeries) = f.df
 
 function Base.show(io::IO, s::FredSeries)
     @printf io "FredSeries\n"
-    @printf io "\tid: %s\n"                    id(s)
-    @printf io "\ttitle: %s\n"        title(s)
-    @printf io "\tunits: %s\n"                 units(s)
-    @printf io "\tseas_adj (native): %s\n"     seas_adj(s)
-    @printf io "\tfreq (native): %s\n"         freq(s)
-    @printf io "\trealtime_start: %s\n"        realtime_start(s)
-    @printf io "\trealtime_end: %s\n"          realtime_end(s)
-    @printf io "\tlast_updated: %s\n" last_updated(s)
-    @printf io "\tnotes: %s\n"        notes(s)
-    @printf io "\ttrans_short: %s\n"  trans_short(s)
+    @printf io "\tid: %s\n"                id(s)
+    @printf io "\ttitle: %s\n"             title(s)
+    @printf io "\tunits: %s\n"             units(s)
+    @printf io "\tseas_adj (native): %s\n" seas_adj(s)
+    @printf io "\tfreq (native): %s\n"     freq(s)
+    @printf io "\trealtime_start: %s\n"    realtime_start(s)
+    @printf io "\trealtime_end: %s\n"      realtime_end(s)
+    @printf io "\tlast_updated: %s\n"      last_updated(s)
+    @printf io "\tnotes: %s\n"             notes(s)
+    @printf io "\ttrans_short: %s\n"       trans_short(s)
     @printf io "\tdf: %dx%d DataFrame with columns %s\n" size(df(s))... names(df(s))
 end
 
