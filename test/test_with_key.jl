8e9f2e735088a48c8198947bd3fcde0699174366
Micah Smith <micahjsmith@gmail.com>
2018-04-27 13:56:18 -0400

    Extend test skeleton

--- test/test_with_key.jl
+++ test/test_with_key.jl
@@ -1,12 +1,37 @@
 using FredData
 using Base.Test
 
-f = Fred()
-s1 = get_data(f, "GDPC1")
-s2 = get_data(f, "GDPC1"; units="chg", vintage_dates="2015-01-01")
+@testset "Basic usage" begin
+    f = Fred()
+    s1 = get_data(f, "GDPC1")
+end
 
-# Bad requests
-@test_throws Exception get_data(f, "GDPC1"; limit="foo")
-@test_throws Exception get_data(f, "GDPC1"; vintage_dates="bar")
+@testset "Sanity tests on simply queries" begin
+    function sanity_test_on_query_response(id)
+        f = Fred()
+        s = get_data(f, id)
+        @test s.id = id
+        @test !isempty(s.data)
+    end
+
+    for id in ["GDPC1"]
+        sanity_test_on_query_response(id)
+    end
+end
+
+@testset "Consistent responses from specific vintages" begin
+    f = Fred()
+    vintage_dates = "2015-01-01"
+    s = get_data(f, "GDPC1"; units="chg", vintage_dates=vintage_dates)
+    @test size(s.data) == (271, 4)
+    @test s.realtime_start = vintage_dates
+    @test s.realtime_end = vintage_dates
+end
+
+@testset "Bad requests throw exceptions" begin
+    f = Fred()
+    @test_throws Exception get_data(f, "GDPC1"; limit="foo")
+    @test_throws Exception get_data(f, "GDPC1"; vintage_dates="bar")
+end
 
 nothing
