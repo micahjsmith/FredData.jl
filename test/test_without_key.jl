aed41a6e604abe2370b0acdc3d7c290ad1a9f8be
Micah Smith <micahjsmith@gmail.com>
2018-04-27 14:57:29 -0400

    Fixes

--- test/test_without_key.jl
+++ test/test_without_key.jl
@@ -1,34 +1,30 @@
 using FredData
 using Base.Test
 
-function with_key_env(f::Function, key::String)
-    key_env_name = FredData.KEY_ENV_NAME
-    withenv(key_env_name => key) do
+function with_key_env(f::Function, key::AbstractString)
+    withenv(FredData.KEY_ENV_NAME => key) do
         f()
     end
 end
 
-function with_key_file(f::Function, key::String)
-    key_file_name = FredData.KEY_FILE_NAME
+function with_key_file(f::Function, key::AbstractString)
     mktempdir() do tmpdir
-        open(joinpath(tmpdir, key_file_name), "w") do f
+        open(joinpath(tmpdir, FredData.KEY_FILE_NAME), "w") do f
             write(f, key)
         end
-        withenv(key_env_name => nothing,
+        withenv(FredData.KEY_ENV_NAME => nothing,
                 "USERPROFILE" => tmpdir,
-                "HOME" => tmpdir, )
-        do
+                "HOME" => tmpdir, ) do
             f()
         end
     end
 end
 
 function with_key_none(f::Function)
-    mktempdir() do tempdir
-        withenv(key_env_name => nothing,
+    mktempdir() do tmpdir
+        withenv(FredData.KEY_ENV_NAME => nothing,
                 "USERPROFILE" => tmpdir,
-                "HOME" => tmpdir, )
-        do
+                "HOME" => tmpdir, ) do
             f()
         end
     end
@@ -40,7 +36,7 @@ end
 
     # pass key directly
     f1 = Fred(fake_key)
-    @test f1.key = fake_key
+    @test f1.key == fake_key
 
     # detect from ENV
     with_key_env(fake_key) do
@@ -53,12 +49,14 @@ end
     # and HOME for *nix
     with_key_file(fake_key1) do
         f3 = Fred()
-        @test f3.key = fake_key1
+        @test f3.key == fake_key1
     end
 end
 
 @testset "Client creation fails with invalid/missing key" begin
-    bad_key_short = repeat("c", convert(Int, round(FredData.API_KEY_LENGTH/2)))
+    bad_key_short = repeat("c",
+                           convert(Int, round(FredData.API_KEY_LENGTH/2)))
+    println(bad_key_short)
     @test_throws Exception Fred(bad_key_short)
 
     # no key anywhere
